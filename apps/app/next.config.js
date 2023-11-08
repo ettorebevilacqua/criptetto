0b4522eac1d8253a86555b5890c842d96b4448b1
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2023-11-08 05:09:20 +0100

    add configs

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   transpilePackages: ['@acme/ui'],
+   experimental: {
+    appDir: true,
+  },
+ async rewrites() {
+    return [
+      {
+        source: "/api/:slug",
+        destination: "http://localhost:3002/:slug",
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
--- apps/app/tailwind.config.js
+++ apps/app/tailwind.config.js
@@ -1,11 +1,17 @@
 const ui = require('@acme/ui/tailwind')
+// const withMT = require("@material-tailwind/react/utils/withMT")
 
-module.exports = {
-  presets: [require('@vercel/examples-ui/tailwind'), ui],
+module.exports =  {
+  presets: [require('@material-tailwind/react'), ui],
   // `ui.content` includes a path to the components that are using tailwind in @acme/ui
   content: ui.content.concat([
     './pages/**/*.{js,ts,jsx,tsx}',
+    './page/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@vercel/examples-ui/**/*.js',
+    '.@material-tailwind/react/**/*..{js,ts,jsx,tsx}',
   ]),
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
